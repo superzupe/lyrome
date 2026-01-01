ae99f5685f3babbee3996dc8a843552894332831
zulfaz <zahrozulfa516@gmail.com>
2026-01-01 15:46:17 +0700

    resolve home to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import LyrcsResults from "./pages/LyricsPage";
 import AddLyrics from "./pages/AddLyrics";
 import NotFound from "./pages/NotFound";
